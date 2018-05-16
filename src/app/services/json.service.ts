129fbf026e1449bb895f37cf048993fea1b53bbf
Ragupathy Rathinasabapathy <e0267619@u.nus.edu>
2018-05-16 13:00:02 +0800

    code commmit on setting header problem

--- src/app/services/json.service.ts
+++ src/app/services/json.service.ts
@@ -3,7 +3,7 @@ import { Http , Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
-//import { IjsonInterface } from './Interface/IjsonInterface';
+import { IjsonInterface } from '../../app/Interface/IjsonInterface';
 
 @Injectable()
 export class JsonService {
